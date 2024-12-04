f756c2fb52247b28921629be086960c111b55f29
Xanadu13 <104714710+iXanadu13@users.noreply.github.com>
2024-12-04 22:41:18 +0800

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -5,7 +5,7 @@ import { cut } from 'nodejs-jieba'
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/Blog/",
+  base: "/",
 //   lang: 'zh-CN',
   locales: {
     "/": {
